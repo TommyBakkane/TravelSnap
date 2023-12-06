8b12e7a19f30cb9f324114a14ca84c6513330b7b
Arild Frøslie <89512731+arildfroslie@users.noreply.github.com>
2023-12-06 14:17:30 +0100

    stuk på jobb. Koden er litt nuka, branch push safe

---
+++ components/user/ProfileGrid.tsx
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from "react";
+import { FIRESTORE_DB } from "../../config/firebase";
+import { collection, getDocs, onSnapshot } from "firebase/firestore";
+import { ScrollView, View, Text, FlatList, Image, StyleSheet, TouchableOpacity } from "react-native";
+
+interface Post {
+  id: string;
+  title: string;
+  image: string;
+  timestamp: string;
+  description: string;
+}
+
+const ProfileGrid = () => {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    const fetchPosts = () => {
+      try {
+        const postsCollection = collection(FIRESTORE_DB, 'user-posts');
+        const unsubscribe = onSnapshot(postsCollection, (snapshot) => {
+          const postsData = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() } as Post));
+          setPosts(postsData);
+        });
+  
+        return () => {
+          unsubscribe();
+        };
+      } catch (error) {
+        console.error('Error fetching posts:', error);
+      }
+    };
+  
+    fetchPosts();
+  }, []); 
+  
+
+  const renderPostItem = ({ item }: { item: Post }) => (
+    <TouchableOpacity style={styles.gridItem}>
+      <Image source={{ uri: item.image }} style={styles.image} />
+      <Text style={styles.title}>{item.title}</Text>
+    </TouchableOpacity>
+  );
+
+  return (
+    <FlatList
+      data={posts}
+      keyExtractor={(item) => item.id}
+      renderItem={renderPostItem}
+      numColumns={3}
+      contentContainerStyle={styles.gridContainer}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  gridContainer: {
+    padding: 4,
+  },
+  gridItem: {
+    flex: 1,
+    margin: 8,
+  },
+  image: {
+    width: "100%",
+    height: 100,
+    borderRadius: 2,
+    resizeMode: "cover",
+  },
+  title: {
+    marginTop: 8,
+    textAlign: "center",
+  },
+});
+
+export default ProfileGrid;
