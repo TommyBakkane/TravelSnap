3ef325622933dfd775d82823be84ab0cb7a2c903
TommyBakkane <toba020@student.kristiania.no>
2023-12-07 12:01:42 +0100

    fix image upload

--- components/UploadPost.tsx
+++ components/UploadPost.tsx
@@ -1,134 +1,117 @@
-// PostUpload.tsx
-
-import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, ActivityIndicator  } from 'react-native';
-import ImagePicker, { ImagePickerResponse, ImageLibraryOptions, MediaType } from 'react-native-image-picker';
+import React, { useState, useEffect } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, ActivityIndicator, Image } from 'react-native';
 import { FIREBASE_APP, FIRESTORE_DB } from '../config/firebase';
-import { addDoc, collection } from 'firebase/firestore';
 import { getStorage, ref as storageRef, uploadBytes, getDownloadURL } from 'firebase/storage';
+import { addDoc, collection } from 'firebase/firestore';
+import * as ImagePicker from 'expo-image-picker';
+import * as ImageManipulator from 'expo-image-manipulator';
 
-const storage = getStorage(FIREBASE_APP)
+const storage = getStorage(FIREBASE_APP);
 
 const UploadPost = () => {
   const [caption, setCaption] = useState('');
-  const [image, setImage] = useState('');
   const [uploading, setUploading] = useState(false);
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
 
-  const options = {
-    title: 'Select Image',
-    mediaType: 'photo' as MediaType,  // Specify the media type as 'photo'
-    storageOptions: {
-      skipBackup: true,
-      path: 'images',
-    },
-  } as ImageLibraryOptions;
-
-  const handleImagePicker = () => {
-    ImagePicker.launchImageLibrary(options, (response) => {
-      const imageResponse = response as ImagePickerResponse;
-  
-      if (!imageResponse.didCancel) {
-        // Check if 'assets' is present and not empty
-        if (imageResponse.assets && imageResponse.assets.length > 0) {
-          const firstAssetUri = imageResponse.assets[0].uri;
-  
-          // Ensure 'setImage' is only called with a valid URI
-          if (firstAssetUri) {
-            setImage(firstAssetUri);
-          } else {
-            console.log('ImagePicker Error: URI is undefined in the response');
-          }
-        } else {
-          console.log('ImagePicker Error: No assets found in the response');
-        }
-      } else {
-        console.log('User cancelled image picker');
+  useEffect(() => {
+    (async () => {
+      const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+      if (status !== 'granted') {
+        console.log('Sorry, we need media library permissions to make this work.');
       }
+    })();
+  }, []);
+
+  const pickImage = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      quality: 1, // Set quality to 1 to avoid double compression
     });
+  
+    if (!result.canceled) {
+      try {
+        const manipResult = await ImageManipulator.manipulateAsync(
+          result.assets[0].uri,
+          [{ resize: { width: 800 } }],
+          { compress: 0.8, format: ImageManipulator.SaveFormat.JPEG }
+        );
+  
+        setSelectedImage(manipResult.uri);
+      } catch (error) {
+        console.error('Error manipulating image:', error);
+      }
+    }
   };
 
-
-  const handleUpload = async () => {
+  const uploadPost = async () => {
+    if (!selectedImage) {
+      console.log('Please select an image first.');
+      return;
+    }
+  
+    setUploading(true);
+  
     try {
-      // Allow the user to pick an image before proceeding with the upload
-      await handleImagePicker();
-
-      // Check if the image was selected
-      if (!image) {
-        console.log('Please select an image');
-        return;
-      }
-
-      // Set uploading to true to show a loading indicator
-      setUploading(true);
-
-      // Convert image to blob(lol)
-      const response = await fetch(image);
+      const response = await fetch(selectedImage);
       const blob = await response.blob();
-
-      // Create a unique file name
-      const fileName = new Date().getTime().toString();
-
-      // Upload image to Firebase storage
-      const imageRef = storageRef(storage, `images/${fileName}`);
+      const imageName = new Date().getTime().toString();
+  
+      const imageRef = storageRef(storage, `images/${imageName}`);
       await uploadBytes(imageRef, blob);
-
-      // Get image URL
-      const imageUrl = await getDownloadURL(imageRef);
-
-      // Add new Firebase document
+  
+      const downloadURL = await getDownloadURL(imageRef);
+  
+      // Assuming you have a 'posts' collection in your Firestore database
       const userPostsCollectionRef = collection(FIRESTORE_DB, 'posts');
       const userPostsDocRef = await addDoc(userPostsCollectionRef, {
-        image: imageUrl,
+        image: selectedImage,
         caption,
         likes: 0,
         dislikes: 0,
         comments: [],
         timestamp: new Date().getTime(),
       });
-
-      console.log('Post uploaded successfully!');
-    } catch (error: any) {
-      console.error(error);
-      alert('Failed to upload post');
+  
+      console.log('Post uploaded successfully:', downloadURL);
+    } catch (error) {
+      console.error('Error uploading post:', error);
     } finally {
-      // Set uploading back to false after upload completion (success or failure)
       setUploading(false);
-      // Reset data after successful or failed upload
-      setCaption('');
-      setImage('');
     }
   };
 
   return (
     <View style={styles.container}>
       <Text style={styles.heading}>Upload a Post</Text>
-
+  
       <TextInput
         style={styles.input}
         placeholder="Title"
         value={caption}
         onChangeText={(text) => setCaption(text)}
       />
-
+  
+      {selectedImage && <Image source={{ uri: selectedImage }} style={{ width: 200, height: 200, marginBottom: 12 }} />}
+  
+      <TouchableOpacity
+        style={styles.pickImageButton}
+        onPress={pickImage}  
+      >
+        <Text style={styles.pickImageButtonText}>Pick an Image</Text>
+      </TouchableOpacity>
+  
       <TouchableOpacity
         style={[styles.uploadButton, { opacity: uploading ? 0.5 : 1 }]}
-        onPress={handleUpload}
+        onPress={uploadPost}
         disabled={uploading}
       >
         <Text style={styles.uploadButtonText}>
           {uploading ? 'Uploading...' : 'Upload'}
         </Text>
       </TouchableOpacity>
-
-      {/* Display a loading indicator while uploading */}
+  
       {uploading && <ActivityIndicator size="large" color="blue" />}
-
-      <TouchableOpacity style={styles.uploadButton} onPress={handleImagePicker}>
-        <Text style={styles.uploadButtonText}>Pick an Image</Text>
-      </TouchableOpacity>
-
-
     </View>
   );
 };
@@ -137,7 +120,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 16,
-    justifyContent: 'center'
+    justifyContent: 'center',
+    alignItems: 'center', // Center items horizontally
   },
   heading: {
     fontSize: 24,
@@ -150,17 +134,31 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     marginBottom: 12,
     paddingHorizontal: 8,
+    width: '80%', // Adjust the width as needed
   },
   uploadButton: {
     backgroundColor: 'blue',
     padding: 12,
     borderRadius: 8,
     alignItems: 'center',
+    width: '80%', // Adjust the width as needed
+    marginTop: 12, // Add spacing
   },
   uploadButtonText: {
     color: 'white',
     fontWeight: 'bold',
   },
+  pickImageButton: {
+    backgroundColor: 'green',
+    padding: 12,
+    borderRadius: 8,
+    alignItems: 'center',
+    width: '80%', // Adjust the width as needed
+    marginTop: 12, // Add spacing
+  },
+  pickImageButtonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
 });
-
 export default UploadPost;
